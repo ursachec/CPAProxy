96341aa0d7a8898fb2417801a4758f5df129f91e
Chris Ballinger <chrisballinger@gmail.com>
2015-07-09 15:01:11 -0700

    Fix build failure during file copy

--- scripts/setup-prerequisites.sh
+++ scripts/setup-prerequisites.sh
@@ -6,5 +6,9 @@ cp "${SDK_PATH}/usr/include/zlib.h" "${ARCH_BUILT_HEADERS_DIR}/zlib.h"
 
 # Copy ptrace.h for the Tor build process
 SDK_PATH_SIM=$(xcrun -sdk macosx --show-sdk-path)
-mkdir "${ARCH_BUILT_HEADERS_DIR}/sys/"
-cp "${SDK_PATH_SIM}/usr/include/sys/ptrace.h" "${ARCH_BUILT_HEADERS_DIR}/sys/"
+SYS_DIR="${ARCH_BUILT_HEADERS_DIR}/sys/"
+
+if [ ! -d "${SYS_DIR}" ]; then
+  mkdir -p "${SYS_DIR}"
+  cp "${SDK_PATH_SIM}/usr/include/sys/ptrace.h" "${SYS_DIR}"
+fi
