dd61b1134171f3464d86ff23cef9594896b1f73d
Chris Ballinger <chrisballinger@gmail.com>
2016-02-18 15:04:53 -0800

    Test refactoring

--- CPAProxyTests/CPAProxyManagerTests.m
+++ CPAProxyTests/CPAProxyManagerTests.m
@@ -10,18 +10,94 @@
 #import "CPAProxyTestCase.h"
 @import CPAProxy;
 
+
 @interface CPAProxyManagerTests : CPAProxyTestCase
 @property (nonatomic, strong, readwrite) CPAProxyManager *proxyManager;
 @property (nonatomic, strong, readwrite) CPAConfiguration *configuration;
+@property (nonatomic) BOOL torIsRunning;
 @end
 
 @implementation CPAProxyManagerTests
+
+/**
+ *  This is required because Tor can only ever be initialized ONCE by an application
+ *  because the thread doesn't get cleaned up after 'exit'.
+ */
+static CPAProxyManager *sharedProxyManager = nil;
 
 - (void)setUp
 {
     [super setUp];
     
-    //[Expecta setAsynchronousTestTimeout:60 * 3]; // 3 minutes. Sometimes Tor takes a long time to bootstrap
+    // only allow one instance of Tor
+    if (sharedProxyManager != nil) {
+        self.proxyManager = sharedProxyManager;
+        return;
+    }
+    self.configuration = [CPAConfiguration configurationWithTorrcPath:self.torrcPath geoipPath:self.geoipPath torDataDirectoryPath:nil];
+    self.configuration.isolateDestinationPort = YES;
+    self.configuration.isolateDestinationAddress = YES;
+    sharedProxyManager = [CPAProxyManager proxyWithConfiguration:self.configuration];
+    self.proxyManager = sharedProxyManager;
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"startTorExpectation"];
+    XCTestExpectation *progressExpectation = [self expectationWithDescription:@"progress"];
+    
+    [self.proxyManager setupWithCompletion:^(NSString *socksHost, NSUInteger socksPort, NSError *error) {
+        XCTAssertNil(error, @"Error setting up");
+        XCTAssertTrue([socksHost length] > 0, @"No SOCKS host string");
+        XCTAssertTrue(socksPort > 0, @"NO SOCKS port");
+        [expectation fulfill];
+    } progress:^(NSInteger progress, NSString *summaryString) {
+        XCTAssertTrue(progress >= 0 && progress <= 100, @"Invalid progress");
+        XCTAssertTrue([summaryString length] > 0, @"No summary String");
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            [progressExpectation fulfill];
+        });
+    } callbackQueue:dispatch_get_main_queue()];
+    
+    [self waitForExpectationsWithTimeout:3*60 handler:^(NSError *error) {
+        if (error) {
+            NSLog(@"Time out Error");
+        }
+    }];
+}
+
+- (void) testSOCKSPortConfiguration {
+    // The callbackQueue must NOT be the main queue because
+    // Expecta blocks the main queue and the tests will fail
+    dispatch_queue_t callbackQueue = dispatch_queue_create("socks callback queue", 0);
+    
+    __block NSString *expectedSocksPortResponse = [NSString stringWithFormat:@"250 SocksPort=localhost:%lu IsolateDestAddr IsolateDestPort", (unsigned long)self.proxyManager.SOCKSPort];
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Test SOCKS Port config"];
+    
+    // Test SOCKS port configuration
+    [self.proxyManager cpa_getConfigurationVariable:@"SOCKSPort" completionBlock:^(NSString *responseString, NSError *error) {
+        XCTAssertNil(error, @"Error getting socks config");
+        XCTAssertTrue([responseString isEqualToString:expectedSocksPortResponse], @"Error SOCKS response string");
+        [expectation fulfill];
+    } completionQueue:callbackQueue];
+    [self waitForExpectationsWithTimeout:30 handler:^(NSError * _Nullable error) {
+        
+    }];
+}
+
+- (void) testSendingHUP {
+    dispatch_queue_t callbackQueue = dispatch_queue_create("socks callback queue", 0);
+    __block NSString *expectedHUPResponse = @"250 OK";
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Test SOCKS Port config"];
+    
+    [self.proxyManager cpa_sendSignal:@"HUP" completionBlock:^(NSString *responseString, NSError *error) {
+        XCTAssertNil(error, @"Error sedning HUP");
+        XCTAssertTrue([responseString isEqualToString:expectedHUPResponse], @"Error HUP response string");
+        [expectation fulfill];
+    } completionQueue:callbackQueue];
+    [self waitForExpectationsWithTimeout:30 handler:^(NSError * _Nullable error) {
+        
+    }];
 }
 
 - (void)tearDown
@@ -30,59 +106,6 @@
     [super tearDown];    
 }
 
-/**
- *  It is currently impossible to run multiple tests that test Tor bootstrap/connection
- *  because cancelling the Tor thread doesn't result in a clean exist and libevent/kqueue
- *  will complain when you try to start the next test.
- *  
- *  Although not quite idempotent, perhaps a better approach will be to connect only once globally
- *  and send the HUP signal when a reload is desired in each test.
- */
-- (void)testSuccessfulProxySetup
-{
-    self.configuration = [CPAConfiguration configurationWithTorrcPath:self.torrcPath geoipPath:self.geoipPath torDataDirectoryPath:nil];
-    self.configuration.isolateDestinationPort = YES;
-    self.configuration.isolateDestinationAddress = YES;
-    self.proxyManager = [CPAProxyManager proxyWithConfiguration:self.configuration];
-    
-    // The callbackQueue must NOT be the main queue because
-    // Expecta blocks the main queue and the tests will fail
-    dispatch_queue_t callbackQueue = dispatch_queue_create("socks callback queue", 0);
-
-    __block NSString *expectedSocksPortResponse = [NSString stringWithFormat:@"250 SocksPort=localhost:%lu IsolateDestAddr IsolateDestPort", (unsigned long)self.proxyManager.SOCKSPort];
-    __block NSString *expectedHUPResponse = @"250 OK";
-    
-    XCTestExpectation *expectation = [self expectationWithDescription:@"startTorExpectation"];
-    
-    [self.proxyManager setupWithCompletion:^(NSString *socksHost, NSUInteger socksPort, NSError *error) {
-        XCTAssertNil(error, @"Error setting up");
-        XCTAssertTrue([socksHost length] > 0, @"No SOCKS host string");
-        XCTAssertTrue(socksPort > 0, @"NO SOCKS port");
-        if (!error) {
-            // Test SOCKS port configuration
-            [self.proxyManager cpa_getConfigurationVariable:@"SOCKSPort" completionBlock:^(NSString *responseString, NSError *error) {
-                XCTAssertNil(error, @"Error getting socks config");
-                XCTAssertTrue([responseString isEqualToString:expectedSocksPortResponse], @"Error SOCKS response string");
-                [self.proxyManager cpa_sendSignal:@"HUP" completionBlock:^(NSString *responseString, NSError *error) {
-                    XCTAssertNil(error, @"Error sedning HUP");
-                    XCTAssertTrue([responseString isEqualToString:expectedHUPResponse], @"Error HUP response string");
-                    [expectation fulfill];
-                } completionQueue:callbackQueue];
-            } completionQueue:callbackQueue];
-        } else {
-            [expectation fulfill];
-        }
-    } progress:^(NSInteger progress, NSString *summaryString) {
-        XCTAssertTrue(progress >= 0 && progress <= 100, @"Invalid progress");
-        XCTAssertTrue([summaryString length] > 0, @"No summary String");
-    } callbackQueue:callbackQueue];
-    
-    [self waitForExpectationsWithTimeout:3*60 handler:^(NSError *error) {
-        if (error) {
-            NSLog(@"Time out Error");
-        }
-    }];
-}
 
 - (void)testTorDataDirectory
 {
