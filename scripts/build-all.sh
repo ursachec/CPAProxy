ac12332a0a6f296fb7e26d8d49ca58165f1ed2ff
rainwolf <rainwolf@submanifold.be>
2016-08-03 13:58:05 +0200

    fix for undefined keccak symbols

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -131,7 +131,7 @@ cd ../
 # Combine binaries of different architectures results
 BINS=(libcrypto.a libssl.a)
 BINS+=(libevent_core.a libevent_pthreads.a libevent_extra.a libevent_openssl.a libevent.a)
-BINS+=(libcurve25519_donna.a libor-crypto.a libtor.a libor-event.a libor.a libor-trunnel.a libed25519_donna.a libed25519_ref10.a)
+BINS+=(libcurve25519_donna.a libor-crypto.a libtor.a libor-event.a libor.a libor-trunnel.a libed25519_donna.a libed25519_ref10.a libkeccak-tiny.a)
 
 NUMBER_OF_BUILT_ARCHS=${#BUILT_ARCHS[@]}
 
